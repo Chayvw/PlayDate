2b59b4f99c26bbef41b2aa534cf035b02c860068
min <pakmk8@gmail.com>
2020-08-07 14:20:49 -0400

    updated user models

---
+++ client/src/containers/SignUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is Play Date's signup page. </h1>
+            </div>
+        );
+    }
+}
+
+export default SignUp;
