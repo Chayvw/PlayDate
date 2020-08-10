4ba90599feed92935f00aa341a04e7b425869f1d
min <pakmk8@gmail.com>
2020-08-10 10:02:40 -0400

    updated usersController and added password hashing

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,16 +6,35 @@ router.get("/api/signup", (req, res) => {
   res.send("Its working");
 });
 
+// router.post("/api/signup", (req, res) => {
+//   db.User.create({
+//     email: req.body.email,
+//     password: req.body.password,
+//   })
+//     .then(function () {
+//       res.redirect(307, "/api/login");
+//     })
+//     .catch(function (err) {
+//       res.status(401).json(err);
+//     });
+// });
+
 router.post("/api/signup", (req, res) => {
-  db.User.create({
-    email: req.body.email,
-    password: req.body.password,
-  })
-    .then(function () {
-      res.redirect(307, "/api/login");
+  db.User.create(req.body)
+    .then((response) => {
+      res.json({
+        error: false,
+        data: response,
+        message: "Success!",
+      });
     })
-    .catch(function (err) {
-      res.status(401).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to create user",
+      });
     });
 });
 
--- models/user.js
+++ models/user.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   email: {type: String, require: true},
   password: {type: String, require:true}
 });
+
+UserSchema.pre('save', function(next){
+  this.password = bcrypt.hashSync(this.password, saltRounds);
+  next();
+  });
 
 const User = mongoose.model("User", UserSchema);
 
