3e1cfc00bed0358ccf5d40dbedc0763c5df4ffa6
min <pakmk8@gmail.com>
2020-08-10 01:39:33 -0400

    updated usersControllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-router.post("/api/signup", function(req, res) {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
+router.get("/api/signup", (req, res) => {
+res.send("Its working")
+});
+
+router.post("/api/signup", (req, res) => {
+  db.User.create({
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then(function () {
+      res.redirect(307, "/api/login");
     })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function(err) {
-        res.status(401).json(err);
-      });
-  });
+    .catch(function (err) {
+      res.status(401).json(err);
+    });
+});
 
 module.exports = router;
