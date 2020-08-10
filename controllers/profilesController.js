cd01c66a6dd2b1544aadd5aedfb9ab59ed202d04
min <pakmk8@gmail.com>
2020-08-09 23:29:28 -0400

    added post routes for profile

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -2,22 +2,41 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-router.get("/api/profile", (req, res) => {
-    db.Profile.find({})
-      .then((response) => {
-        res.json({
-          error: false,
-          data: response,
-          message: "Success!",
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          error: true,
-          data: null,
-          message: "Unable to retrieve profile",
-        });
+router.get("/api/profiles", (req, res) => {
+  db.Profile.find({})
+    .then((response) => {
+      res.json({
+        error: false,
+        data: response,
+        message: "Success!",
       });
-  });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to retrieve profile",
+      });
+    });
+});
+
+router.post("/api/profiles", (req, res) => {
+  db.Profile.create(req.body)
+    .then((response) => {
+      res.json({
+        error: false,
+        data: response,
+        message: "Success!",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to create profile",
+      });
+    });
+});
 
 module.exports = router;
