f7a369ff020def4b85142cb2e07627554fb2534d
Chay Williams <Chayvw@gmail.com>
2020-08-10 14:19:27 -0400

    name change

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     User: require("./UserModel"),
-    Profile: require("./Profile"),
-    PlayDate: require("./PlayDate")
+    Profile: require("./ProfileModel"),
+    PlayDate: require("./PlayDateModel")
   };
   
