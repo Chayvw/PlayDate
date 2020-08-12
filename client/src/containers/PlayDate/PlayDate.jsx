0fcd50ab8172bd5d6795e9b442f048f75d5bcaf5
Dwight Huggins <dwight_huggins@icloud.com>
2020-08-12 13:00:04 -0400

    updated files

--- client/src/containers/PlayDate/PlayDate.jsx
+++ client/src/containers/PlayDate/PlayDate.jsx
@@ -1,79 +1,145 @@
-import React from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
-import Card from "@material-ui/core/Card";
-import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+class PlayDate extends Component {
+  state = {
+    playdates: [],
+  };
 
-function PlayDate(props) {
-  const [state, setState] = React.useState({
-    events: [
-      {
-        title: "Park Meetup",
-        location: "Atlanta, GA",
-        date: "August 12, 2020",
-        description: "",
-      },
-    ],
-  });
-
-  React.useEffect(() => {
+  componentDidMount() {
     axios
       .get("/api/playdate")
       .then((response) => {
         console.log(response.data);
-        setState({
-          events: response.data.data,
+        this.setState({
+          playdates: response.data.data,
         });
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }
 
-  return (
-    <Container>
-      <Card raised>
-        <Box m={2} textAlign="center">
-          <h1>This is Play Date's main page. </h1>
-          <Box border={1}>
-            {state.events.map((event, i) => (
-              <Grid container key={i + "-events"}>
-                <Grid item xs={12} sm={6}>
-                  Title:
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  {event.title}
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  Location:
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  {event.location}
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  Date:
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  {event.date}
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                <Typography align="right">
-                  Description:
-                  </Typography>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  {event.description}
-                  </Grid>
-              </Grid>
-            ))}
-          </Box>
-        </Box>
-      </Card>
-    </Container>
-  );
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h1>Play Date's</h1>
+          <ul className="list-group">
+            <div>
+              {this.state.playdates.map((playdate) => (
+                <div key={playdate._id}>
+                  <li className="list-group-item">
+                    {playdate.name},{playdate.date},{playdate.location}
+                  </li>
+                </div>
+              ))}
+            </div>
+            <div>
+              {this.state.playdates.length === 0 && <p>no play dates</p>}
+            </div>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  //   getPlayDate = () => {
+  //     axios
+  //       .get("/api/playdate")
+  //       .then((response) => {
+  //         this.setState({
+  //           playdates: response.data.data,
+  //         });
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   };
+
+  // import React from "react";
+  // import axios from "axios";
+  // import { makeStyles } from '@material-ui/core/styles';
+  // import Card from '@material-ui/core/Card';
+  // import CardActionArea from '@material-ui/core/CardActionArea';
+  // import CardActions from '@material-ui/core/CardActions';
+  // import CardContent from '@material-ui/core/CardContent';
+  // import CardMedia from '@material-ui/core/CardMedia';
+  // import Button from '@material-ui/core/Button';
+  // import Typography from '@material-ui/core/Typography';
+  // import Grid from '@material-ui/core/Grid';
+
+  // const useStyles = makeStyles({
+  //   root: {
+  //     maxWidth: 345,
+  //     padding: '50px',
+  //   },
+
+  // });
+
+  // function PlayDate(props) {
+  //   const classes = useStyles();
+  //   const [state, setState] = React.useState({
+  //     events: [
+  //       {
+  //         title: "Park Meetup",
+  //         location: "Atlanta, GA",
+  //         date: "August 12, 2020",
+  //         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere reiciendis quisquam fugiat, tempore ab, consectetur, laborum ipsum hic atque corporis itaque sapiente minima. Qui molestias itaque maxime. Ut, necessitatibus quos!",
+  //       },
+  //     ],
+  //   });
+
+  //   React.useEffect(() => {
+  //     axios
+  //       .get("/api/playdate")
+  //       .then((response) => {
+  //         console.log(response.data);
+  //         setState({
+  //           events: response.data.data,
+  //         });
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }, []);
+
+  //   return (
+
+  //         <Card className={classes.root} width={1/4}>
+  //       <CardActionArea>
+  //       {state.events.map((event, i) => (
+  //               <Grid container key={i + "-events"}>
+  //                 <Grid item xs={12} sm={6}>
+  //         <CardContent>
+  //           <Typography gutterBottom variant="h4" component="h1">
+  //             {event.title}
+  //           </Typography>
+  //           <Typography gutterBottom variant="h6" component="h2">
+  //             {event.location}
+  //           </Typography>
+  //           <Typography gutterBottom variant="body1" component="h2">
+  //             {event.date}
+  //           </Typography>
+  //           <Typography variant="body2" color="textSecondary" component="p">
+  //             {event.description}
+  //           </Typography>
+  //         </CardContent>
+  //         </Grid>
+  //         </Grid>
+  //       ))}
+  //       </CardActionArea>
+  //       <CardActions>
+  //         <Button size="small" color="primary">
+  //           Share
+  //         </Button>
+  //         <Button size="small" color="primary">
+  //           Email
+  //         </Button>
+  //       </CardActions>
+  //     </Card>
+
+  //   );
 }
 
 export default PlayDate;
--- client/src/containers/SignUp/SignUp.jsx
+++ client/src/containers/SignUp/SignUp.jsx
@@ -1,102 +1,30 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { Component } from "react";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export default function SignIn() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
+class SignUp extends Component {
+  render() {
+    return (
+      <div className="container">
+        <form class="text-center border border-light p-5">
+          <p class="h4 mb-4">SignUp</p>
+          <input
+            type="email"
+            id="defaultLoginFormEmail"
+            class="form-control mb-4"
+            placeholder="E-mail"
           />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
+          <input
             type="password"
-            id="password"
-            autoComplete="current-password"
+            id="defaultLoginFormPassword"
+            class="form-control mb-4"
+            placeholder="Password"
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-            </Grid>
-            <Grid item>
-            </Grid>
-          </Grid>
+          <button type="submit" className="btn btn-light-blue">
+            Submit
+          </button>
         </form>
       </div>
-    </Container>
-  );
+    );
+  }
 }
+
+export default SignUp;
