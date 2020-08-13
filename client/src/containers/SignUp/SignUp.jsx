dfda89f1c63a13ed0b00c79852b9dfde8765cd30
Dwight Huggins <dwight_huggins@icloud.com>
2020-08-13 11:59:06 -0400

    signup folder

--- client/src/containers/SignUp/SignUp.jsx
+++ client/src/containers/SignUp/SignUp.jsx
@@ -1,19 +1,51 @@
 import React, { Component } from "react";
+import API from "../../utils/API";
 
 class SignUp extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleChange = (e) => {
+    console.log(e.target)
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submitForm = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+
+    API.signUp(this.state)
+      .then((response) => {
+        console.log(response);
+        this.props.history.push("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div className="container">
-        <form className="text-center border border-light p-5">
+        <form
+          className="text-center border border-light p-5"
+          onSubmit={this.submitForm}
+        >
           <p className="h4 mb-4">SignUp</p>
           <input
             type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
             id="defaultLoginFormEmail"
-            class="form-control mb-4"
+            className="form-control mb-4"
             placeholder="E-mail"
           />
           <input
             type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
             id="defaultLoginFormPassword"
             className="form-control mb-4"
             placeholder="Password"
@@ -28,106 +60,3 @@ class SignUp extends Component {
 }
 
 export default SignUp;
-
-// import React from 'react';
-// import Button from '@material-ui/core/Button';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
-// import Typography from '@material-ui/core/Typography';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Container from '@material-ui/core/Container';
-
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://material-ui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
-
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   form: {
-//     width: '100%', // Fix IE 11 issue.
-//     marginTop: theme.spacing(1),
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//   },
-// }));
-
-// export default function SignIn() {
-//   const classes = useStyles();
-
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <div className={classes.paper}>
-//         <Typography component="h1" variant="h5">
-//           Sign in
-//         </Typography>
-//         <form className={classes.form} noValidate>
-//           <TextField
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             id="email"
-//             label="Email Address"
-//             name="email"
-//             autoComplete="email"
-//             autoFocus
-//           />
-//           <TextField
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             name="password"
-//             label="Password"
-//             type="password"
-//             id="password"
-//             autoComplete="current-password"
-//           />
-//           <FormControlLabel
-//             control={<Checkbox value="remember" color="primary" />}
-//             label="Remember me"
-//           />
-//           <Button
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             className={classes.submit}
-//           >
-//             Sign In
-//           </Button>
-//           <Grid container>
-//             <Grid item xs>
-//             </Grid>
-//             <Grid item>
-//             </Grid>
-//           </Grid>
-//         </form>
-//       </div>
-//     </Container>
-//   );
-// }
