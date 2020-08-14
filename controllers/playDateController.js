4404d62478d6981a5a434fed894cc28af5192771
Chay Williams <Chayvw@gmail.com>
2020-08-14 14:19:55 -0400

    were able to connect play dates to user

--- controllers/playDateController.js
+++ controllers/playDateController.js
@@ -4,163 +4,122 @@ const db = require("../models");
 const jwt = require("jsonwebtoken")
 
 // Create
-    router.post("/api/playdate", (req, res) => {
-        console.log(req.headers.auth);
-        jwt.verify(req.headers.auth, process.env.JWT_PASSWORD, function(err, decoded) {
-            if(err) {
-                console.log("Decoding token error");
-                console.log(err);
-            }else {
-                console.log(decoded)
-            }
-          });
-    
-  db.PlayDate.create(req.body)
-    .then((createdPlayDate) => {
-      res.json({
-        error: false,
-        data: createdPlayDate,
-        message: "Successfully created new play date",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: true,
-        data: null,
-        message: "Unable to create play date",
-      });
+router.post("/api/playdate", (req, res) => {
+    console.log(req.headers.auth);
+    jwt.verify(req.headers.auth, process.env.JWT_PASSWORD, function (err, decoded) {
+        if (err) {
+            console.log("Decoding token error");
+            console.log(err);
+        } else {
+            console.log("decoded", decoded)
+            db.PlayDate.create(req.body)
+                .then((createdPlayDate) => {
+                    db.User.findOneAndUpdate(
+                        { _id: decoded.id },
+                        { $push: { playdates: createdPlayDate.id } },
+                        { new: true }
+                    )
+                        .populate("playdates")
+                        .then((updatedUser) => {
+                            res.json({
+                                error: false,
+                                data: updatedUser,
+                                message: "Successfully created new play date",
+                            });
+                        });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json({
+                        error: true,
+                        data: null,
+                        message: "Unable to create play date",
+                    });
+                });
+        }
     });
+
+
 });
 // Read
 router.get("/api/playdate", (req, res) => {
-  db.PlayDate.find({})
-    .then((foundPlaydate) => {
-      res.json({
-        error: false,
-        data: foundPlaydate,
-        message: "Successfully found all play dates.",
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: true,
-        data: null,
-        message: "Unable to retrieve all play date ",
-      });
-    });
+    db.PlayDate.find({})
+        .then((foundPlaydate) => {
+            res.json({
+                error: false,
+                data: foundPlaydate,
+                message: "Successfully found all play dates.",
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: true,
+                data: null,
+                message: "Unable to retrieve all play date ",
+            });
+        });
 });
 // Update
 router.put("/api/playdate/:id", (req, res) => {
-  console.log(req.body, req.params);
-  db.PlayDate.updateOne(
-    {
-      _id: req.params.id,
-    },
-    {
-      $set: {
-        name: req.body.name,
-        date: req.body.date,
-        location: req.body.location,
-        description: req.body.description,
-        //   modified: Date.now()
-      },
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
+    console.log(req.body, req.params);
+    db.PlayDate.updateOne(
+        {
+            _id: req.params.id,
+        },
+        {
+            $set: {
+                name: req.body.name,
+                date: req.body.date,
+                location: req.body.location,
+                description: req.body.description,
+                //   modified: Date.now()
+            },
+        },
+        (error, data) => {
+            if (error) {
+                res.send(error);
+            } else {
+                res.send(data);
+            }
+        }
+    );
 });
 
 // get route by id
 router.get("/api/playdate/:id", (req, res) => {
-  console.log(req.body, req.params);
-  db.PlayDate.findOne({ _id: req.params.id }).then((response) => {
-    res.json({
-      error: false,
-      data: response,
-      message: "Success!",
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json({
-      error: true,
-      data: null,
-      message: "Unable to get playdate by id",
-    });
-  });
+    console.log(req.body, req.params);
+    db.PlayDate.findOne({ _id: req.params.id }).then((response) => {
+        res.json({
+            error: false,
+            data: response,
+            message: "Success!",
+        });
+    })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: true,
+                data: null,
+                message: "Unable to get playdate by id",
+            });
+        });
 });
 
 router.delete("/api/playdate/:id", (req, res) => {
-  db.PlayDate.deleteOne(
-    {
-      _id: req.params.id,
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
+    db.PlayDate.deleteOne(
+        {
+            _id: req.params.id,
+        },
+        (error, data) => {
+            if (error) {
+                res.send(error);
+            } else {
+                res.send(data);
+            }
+        }
+    );
 });
 
-// router.get("/api/ingredients/:id", (req, res) => {
-//     db.Ingredient.findById(req.params.id)
-//       .then((foundPlaydate) => {
-//         res.json({
-//           error: false,
-//           data: foundIngredient,
-//           message: "Successfully retrieved play dates.",
-//         });
-//       })
-//       .catch((err) => {
-//         res.status(500).json({
-//           error: true,
-//           data: null,
-//           message: "Unable to retrieve play dates . ",
-//         });
-//       });
-//   });
 
-// .post("/submit", ({ body }, res) => {
-//     db.Note.create(body)
-//       .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-//       .then(dbUser => {
-//         res.json(dbUser);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
-
-//   app.get("/populateduser", (req, res) => {
-//     db.User.find({})
-//       .populate("notes")
-//       .then(dbUser => {
-//         res.json(dbUser);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
-
-// router.post("/api/playdate", (req, res) => {
-//     const id = "5f3198a236bd9ece99c980dd"
-//     db.PlayDate.create(req.body)
-//         .then(({ _id }) => db.User.findOneAndUpdate({_id: id}, { $push: { playdates: _id } }, { new: true }))
-//         .then(dbUser => {
-//             res.json(dbUser);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
 
 module.exports = router;
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,18 +6,6 @@ router.get("/api/signup", (req, res) => {
   res.send("Its working");
 });
 
-// router.post("/api/signup", (req, res) => {
-//   db.User.create({
-//     email: req.body.email,
-//     password: req.body.password,
-//   })
-//     .then(function () {
-//       res.redirect(307, "/api/login");
-//     })
-//     .catch(function (err) {
-//       res.status(401).json(err);
-//     });
-// });
 
 router.post("/api/signup", (req, res) => {
   db.User.create(req.body)
@@ -34,38 +22,6 @@ router.post("/api/signup", (req, res) => {
     });
 });
 
-// router.post("/api/login", (req, res) => {
-//   db.User.findOne({
-//     email: req.body.email,
-//   })
-//     .then((dbModel) => {
-//       if (!dbModel) {
-//         return res.json("Email or password is incorrect");
-//       }
-//       else if (!dbModel.validPassword(req.body.password)) {
-//         return res.json("Email or password is incorrect")
-//       }
-//       else res.json(dbModel);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({
-//         error: true,
-//         data: null,
-//         message: "Unable to create user",
-//       });
-//     });
 
-    // router.get("/api/populateduser", (req, res) => {
-    //   db.User.find({})
-    //     .populate("playdate")
-    //     .then(dbUser => {
-    //       res.json(dbUser);
-    //     })
-    //     .catch(err => {
-    //       res.json(err);
-    //     });
-    // });
-// });
 
 module.exports = router;
